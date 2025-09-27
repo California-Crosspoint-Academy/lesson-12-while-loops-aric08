99f1b23b1d413de8cc42023c30997895b5dbce2f
howieju <howieju@gmail.com>
2025-09-27 09:42:09 -0700

    Update mcq.java

--- mcq.java
+++ mcq.java
@@ -1,10 +1,10 @@
 public class mcq {
     public static void main(String[] args) {
 
-        System.out.println("Q1: Both A and B");
+        System.out.println("Q1: Both A and B"); //// correct answer D
         System.out.println("Q2: Infinite number of times");
-        System.out.println("Q3: None of these (-1)");
-        System.out.println("Q4: Can't be determined");
+        System.out.println("Q3: None of these (-1)"); ////  correct answer A
+        System.out.println("Q4: Can't be determined"); ////  correct answer E
         System.out.println("Q5: Both A and B");
     }
 }
